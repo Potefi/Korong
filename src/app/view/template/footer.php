4752f127aee19fc52287e781134b8fc15a49af73
Szabo Zalan <zazi.szabo@gmail.com>
2021-03-17 23:00:40 +0100

    v0.4.3.1 Footer - My Account fix

--- src/app/view/template/footer.php
+++ src/app/view/template/footer.php
@@ -1,6 +1,6 @@
 <div id="footer" class="container-fluid bg-dark text-center">
     <a href="/zarodolgozat/?controller=categories">Kategóriák</a> |
-    <a href="/zarodolgozat/?controller=user&action=<?= isset($_SESSION['userid'])?'account':'login' ?>">Fiókom</a> |
+    <a href="/zarodolgozat/?controller=user&action=<?= isset($_SESSION['userId'])?'account':'login' ?>">Fiókom</a> |
     <a href="/zarodolgozat/?controller=contactDetails">Kapcsolat</a> |
     <a href="/zarodolgozat/?controller=contactDetails">ÁSZF</a><br>
     <p>Copyright © 2021, Korong lemezbolt</p>
